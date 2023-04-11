d22b3e141d8b85edf6df3928b73e8e2fba225617
nestorgonzalez <98nestor1998@gmail.com>
2023-04-11 19:51:51 +0200

    create_post api route created

--- Toto/api/routes.py
+++ Toto/api/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, Response
+from pymongo.errors import OperationFailure
 
 import Toto.database.db as db
 from Toto.utils.logs import logger
@@ -11,14 +12,19 @@ def index():
     return "<p>This is the access point of the api<p>"
 
 #Create Post
+#TODO: Check if the collection's name (board)
 bp_create_post = Blueprint("create_post", __name__, url_prefix="/api")
 
 @bp_create_post.route("/create_post", methods = ['POST'])
 def create_post():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
+        board = request.args.get('board')
         json = request.json
-        return json
+        database = db.mongo["TotoDB"]
+        collection = database[board]
+        collection.insert_one(json)
+        return "Inserted successfully!\n"
     else:
         logger.warning('Invalid post content type!')
         return Response("Error 400: Bad request\n", status=400)
